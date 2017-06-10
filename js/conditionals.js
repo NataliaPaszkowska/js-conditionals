fd5d871004508408df7a3dafc84f359a53333355
Natalia Paszkowska <npaszkowska@gmail.com>
2017-06-10 12:35:45 +0200

    commit

--- js/conditionals.js
+++ js/conditionals.js
@@ -1,1 +1,43 @@
 'use strict'
+
+/*var wzrostMateusza = 170;
+var wzrostOlgi = 190;*/
+
+/*if (wzrostOlgi < wzrostMateusza) {
+    console.log("Olga jest nizsza :)");
+}*/
+
+/*
+if (wzrostOlgi < wzrostMateusza) {
+    console.log("Olga jest nizsza :)");
+    
+} else { 
+    console.log("Olga jest wyzsza");
+}*/
+
+
+/*if (wzrostOlgi > wzrostMateusza) {
+    console.log("Olga jest wyzsza :)");
+
+} else if (wzrostOlgi == wzrostMateusza) {
+    console.log("Olga jest taka sama");
+} else {
+    console.log("Olga jest nizsza");
+}*/
+
+/* switch dziala tak samo jako if else */
+
+var kolor = 'zielony';
+switch (kolor) {
+    case 'czerwony':
+        console.log('kolor czerwony');
+        break;
+    case 'zielony':
+        console.log('kolor zielony');
+        break;
+    case 'niebieski':
+        console.log('kolor niebieski');
+        break;
+    default:
+        console.log('inny kolor');
+             }
